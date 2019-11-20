d8e7cfa1f013d5b2d68cddd61a45d3ecfc78450f
haruka-g <mortndpgj@gmail.com>
2019-11-20 22:38:31 +0900

    routerの読み込みやstaticファイルのセット個所を保留にした

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const router = require('./router');
+//router/index.jsを実装したらここで読み込む
 const app = express();
 const PORT = 3001;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use('/', router);
+//静的ファイルを実装したら保存場所をセットする
+//router/index.jsを実装したらルーティングをセットする
 
 app.listen(PORT, () => {
   console.log(`Quiz app listening on port ${PORT}`);
